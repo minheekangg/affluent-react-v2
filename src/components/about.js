cf340bd2b6b611aedd0fdb6b7986e22e8a0e9c64
minheekangg <minheekangg@gmail.com>
2019-03-29 21:31:56 -0400

    about page job change done

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from "semantic-ui-react";
+
+import Team from './team'
 
 import shark from '../images/shark.png'
 
@@ -18,9 +21,29 @@ class About extends React.Component{
             <span><h3 style={{fontWeight: "bold"}}>We care. <br/>
                 Perhaps too much.</h3>
             </span>
-                
             <div id="image-container"><img src={shark} alt="test" /></div>
             </div>
+
+            <Grid.Row id="aboutpage-2" style={{textAlign: "center"}}>
+            <h4>Bringing ethical lending back</h4>
+                <div id="ethical-table">
+                   <div><h3>Accessible financing</h3>We position ourselves as an alternative option to the payday or personal loan industry. Affluent provides simple financing options with quick access to cash.</div>
+                   <div><h3>Surviving a loan</h3>Traditional consumer lenders have made a business trapping borrowers into debt. We want to change that, so we pride ourselves on transparent terms and making sure you know what you’re paying.</div>
+                </div>
+                <div className="line"></div>
+                <div id="ethical-table">
+                   <div><h3>Improving credits</h3>Helping our borrowers solve their cash emergencies is important, but we also want to facilitate our borrowers to improve their credit scores. </div>
+                   <div><h3>Innovation</h3>Using machine-learning and new technology, we are able to offer better rates and terms than traditional lenders.</div>
+                </div>
+              </Grid.Row> 
+                <div id="team-div">
+                    <h1><span style={{ color: 'var(--color-lightgreen)', fontFamily: "var(--main-font)"}}>affluent's</span> team is made up of</h1>
+                    <Team />
+                    <br/>
+                    <div className="line2"></div>
+                </div>
+        
+
 
           </>
         );
---
+++ src/components/team.js
@@ -0,0 +1,28 @@
+import React from 'react'; 
+
+
+class Team extends React.Component {
+  state = {
+    currentJobIdx: 0
+  };
+
+
+  render() {
+      const jobs = ["entrepreneurs", "software engineers", "dreamers", "FRANCO"];
+      setTimeout(() => {
+          if (this.state.currentJobIdx < jobs.length -1) {
+              this.setState({ currentJobIdx: this.state.currentJobIdx+1 })
+          } else {
+              this.setState({ currentJobIdx: 0})
+          }
+          ;
+      }, 3000);
+
+    return (
+      <div id="team-box">
+        {jobs[this.state.currentJobIdx]}
+      </div>
+    )
+  }
+}
+export default Team;
