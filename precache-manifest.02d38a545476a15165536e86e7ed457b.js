e5be95f23b4b7c2d4c2dc1c29f02d27a28694f10
minheekangg <minheekangg@gmail.com>
2019-04-03 22:57:32 -0400

    Updates

---
+++ precache-manifest.02d38a545476a15165536e86e7ed457b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "74dc15f2a8bdd82c366c",
+    "url": "/minheekangg/affluent-react-v2/static/css/main.0e3d435c.chunk.css"
+  },
+  {
+    "revision": "74dc15f2a8bdd82c366c",
+    "url": "/minheekangg/affluent-react-v2/static/js/main.d80aedea.chunk.js"
+  },
+  {
+    "revision": "725bf9054d4f5bdfcfef",
+    "url": "/minheekangg/affluent-react-v2/static/js/runtime~main.f55448dd.js"
+  },
+  {
+    "revision": "e9ea7ae323e7a8234383",
+    "url": "/minheekangg/affluent-react-v2/static/css/2.0450462f.chunk.css"
+  },
+  {
+    "revision": "e9ea7ae323e7a8234383",
+    "url": "/minheekangg/affluent-react-v2/static/js/2.b13d4c17.chunk.js"
+  },
+  {
+    "revision": "18aa054e0adf08f16dd647c58421e07d",
+    "url": "/minheekangg/affluent-react-v2/static/media/1.18aa054e.png"
+  },
+  {
+    "revision": "7f63f525dcef266a969c14dfcc58e738",
+    "url": "/minheekangg/affluent-react-v2/static/media/2.7f63f525.png"
+  },
+  {
+    "revision": "a9620c6008c75995f8147273029382dd",
+    "url": "/minheekangg/affluent-react-v2/static/media/3.a9620c60.png"
+  },
+  {
+    "revision": "c94742f988365e2f4a41b32450ffe46e",
+    "url": "/minheekangg/affluent-react-v2/static/media/1_active.c94742f9.png"
+  },
+  {
+    "revision": "a0ac1036d9b8d5141e4cb484e46aec65",
+    "url": "/minheekangg/affluent-react-v2/static/media/2_active.a0ac1036.png"
+  },
+  {
+    "revision": "7d9aee7e01603c02939b1a444d9ec91a",
+    "url": "/minheekangg/affluent-react-v2/static/media/3_active.7d9aee7e.png"
+  },
+  {
+    "revision": "615618b603e71ad8ccee5d0986ecd9e6",
+    "url": "/minheekangg/affluent-react-v2/static/media/sickpupper.615618b6.png"
+  },
+  {
+    "revision": "4288b274b1d62feca92e37864a451184",
+    "url": "/minheekangg/affluent-react-v2/static/media/graph.4288b274.png"
+  },
+  {
+    "revision": "f98ceefc5acf62b5a9c3149f1fd2bb98",
+    "url": "/minheekangg/affluent-react-v2/static/media/shark.f98ceefc.png"
+  },
+  {
+    "revision": "cbfc9a6079bd16032867d8dbf48b4146",
+    "url": "/minheekangg/affluent-react-v2/index.html"
+  }
+];
